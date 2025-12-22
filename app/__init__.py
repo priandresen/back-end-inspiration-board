00d993be52a97ef8b3daef53822b4a97ee787389
Priscilla Andresen <pgdamorim@gmail.com>
2025-12-22 13:11:29 -0800

    initial migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from .db import db, migrate
+from .models import card, board
 import os
 # Import models, blueprints, and anything else needed to set up the app or database
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
@@ -14,8 +17,11 @@ def create_app(config=None):
         app.config.update(config)
 
     # Initialize app with SQLAlchemy db and Migrate
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-    # Register Blueprints 
+    # Register Blueprints
+
 
     CORS(app)
     return app
