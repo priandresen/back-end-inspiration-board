e9e36c9919c39d17f614acd3798efa1e8d6310c0
Priscilla Andresen <pgdamorim@gmail.com>
2026-01-02 10:51:34 -0800

    delete route for board

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -14,3 +14,10 @@ def get_one_board(board_id):
 def create_board():
     request_body = request.get_json()
     return create_model(Board, request_body)
+
+@bp.delete("/<board_id>")
+def delete_board(board_id):
+    board = validate_model(Board, board_id)
+    db.session.delete(board)
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
