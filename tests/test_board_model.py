9c409a844fa7eb828824534d907aa5247f2015aa
Anaiah <amaddox.dev@gmail.com>
2026-01-08 00:56:14 -0800

    Added tests to ensure boards with missing title or owner fields are not created

--- tests/test_board_model.py
+++ tests/test_board_model.py
@@ -1,6 +1,39 @@
 from app.models.board import Board
-# from app.models.card import Card
+from app.db import db
 import pytest
+
+def test_board_creation():
+    #Arrange
+    title = "2026 Goals"
+    owner = "Super Motivated Student"
+    board = Board(title=title, owner=owner)
+    db.session.add(board)
+    db.session.commit()
+    #Act
+    board = Board.query.get(board.id)
+    #Assert
+    assert board.id is not None
+    assert board.id == board,id
+    assert board.title == title
+    assert board.owner == owner
+
+def test_board_missing_title_not_created():
+    #Arrange
+    board = Board(owner="Nesferatu")
+    db.session.add(board)
+    #Act
+    db.session.commit()
+    #Assert
+    assert board.id is None, "Board without title should not be created"
+
+def test_board_missing_owner_not_created():
+    #Arrange
+    board = Board(title="Spooky Board")
+    db.session.add(board)
+    #Act
+    db.session.commit()
+    #Assert
+    assert board.id is None, "Board without owner should not be created"
 
 def test_board_to_dict(one_saved_board):
     #Arrange
